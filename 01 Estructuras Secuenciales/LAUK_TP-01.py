5ae85b6c4c98007bcb5dcde1845e10da7e77634a
karenlauk <karenlauktecnica@gmail.com>
2025-04-01 00:23:45 -0300

    Subiendo el tp1

---
+++ 01 Estructuras Secuenciales/LAUK_TP-01.py
@@ -0,0 +1,63 @@
+#Actividades
+#1)
+print (f"Hola Mundo");
+print (f"Adíos mundo")
+
+#2)
+# nombre = input ("Ingrese su nombre: ");
+# print (f"Hola {nombre}!");
+
+#3)
+#print("Complete lo siguiente con sus datos: ");
+#nombre = input("Nombre: ");
+#apellido = input("Apellido: ");
+#edad = input("Edad: ");
+#residencia = input("Lugar de residencia: ");
+#print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.");
+
+#4)
+#radio = float(input("Ingrese el radio de un círculo: "));
+#area = 3.14 * radio**2 ;
+#perimetro = 2 * 3.14 * radio;
+#print(f"El área del círculo es {area} y su perímetro es {perimetro}");
+
+#5)
+#segundos =int(input("Ingrese una cantidad de segundos: "));
+#hora =  segundos / 3600;
+#print (f"{segundos} segundos equivalen a {hora} horas.");
+
+#6)
+#numero = int(input("Ingrese un número: "));
+#print(f"Tabla del {numero}:")
+#for i in range (1, 11):
+#    print(f"{numero} x {i} = {numero * i}"
+
+#7)
+#numero1 = int(input("Ingrese el primer número entero distinto de 0: "));
+#numero2 = int(input("Ingrese el segundo número entero distinto de 0: "));
+#suma = numero1 + numero2;
+#resta = numero1 - numero2;
+#multiplicación = numero1 * numero2;
+#división = numero1 / numero2;
+#print(f"Suma: {suma}"); 
+#print(f"Resta: {resta}");
+#print(f"Multiplicación: {multiplicación}");
+#print(f"División: {división}");
+
+#8)
+#peso = float(input("Ingrese su peso en kg: "));
+#altura = float(input("Ingrese su altura en metros: "));
+#masa = peso / (altura ** 2);
+#print(f"Su Índice de Masa Corporal (IMC) es: {masa}");
+
+#9)
+#celsius = float(input("Ingrese la temperatura en grados Celsius: "));
+#fahrenheit = (9/5) * celsius + 32;
+#print(f"{celsius}°C equivale a {fahrenheit}°F");
+
+#10)
+#numero1 = float(input("Ingrese el primer número: "));
+#numero2 = float(input("Ingrese el segundo número: "));
+#numero3 = float(input("Ingrese el tercer número: "));
+#promedio = (numero1 + numero2 + numero3) / 3;
+#print(f"El promedio de los números ingresados es: {promedio}");
