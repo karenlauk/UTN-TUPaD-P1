76302215729148b1e507abbf264295b463af3c43
KarenLauk10 <153031583+KarenLauk10@users.noreply.github.com>
2025-05-09 03:10:19 -0300

    Entrega del trabajo N°5

---
+++ 05 Funciones/LAUK_TP-05.py.py
@@ -0,0 +1,54 @@
+#Trabajo Práctico N°4 - Listas
+#Ejercicios:
+#1)
+#multiplo = list(range(4, 101, 4));
+#print(multiplo);
+
+#2)
+#gustos = ["deportes", "música", "viajar", "entrenar", "leer"];
+#print(gustos[-2]);
+
+#3)
+#lista = [];
+#lista.append("tomate");
+#lista.append("papas");
+#lista.append("lechuga");
+#print(lista);
+
+#4)
+#animales = ["perro", "gato", "conejo", "pez"];
+#animales[1] = "loro";
+#animales[-1] = "oso";
+#print(animales);
+
+#5) Explicación de la imagen
+#En la primera línea, tenemos una lista de números con diferentes valores.; 
+#En la segunda línea, la lista se ve que la acompaña el remove(max(numeros)), donde “max” busca el valor máximo dentro de la lista, y el “remove” para borrarlo.;
+#En la tercera línea, se imprime la lista sin él número máximo.;
+
+#6) Lista
+#lista = list(range(10, 31, 5));
+#print(lista[:2]);
+
+#7)
+#autos = ["prisma", "civic", "gol", "tracker"];
+#autos[1:3] = ["208", "jeep"];
+#print(autos); 
+
+#8)
+#dobles = [];
+#dobles.append(5 * 2);
+#dobles.append(10 * 2);
+#dobles.append(15 * 2);
+#print(dobles);
+
+#9)
+#compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]];
+#compras[2].append("jugo");
+#compras[1][1] = "tallarines";
+#compras[0].remove("pan"); 
+#print(compras);
+
+#10)
+lista_anidada =[[15],["True"], [25.5, 57.9, 30.6], ["False"]];
+print(lista_anidada);
